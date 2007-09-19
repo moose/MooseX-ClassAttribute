4dee0fd36e42cb0e22e39a652bd931197a72ccc3
Dave Rolsky <autarch@urth.org>
2007-09-19 20:00:33 +0000

    Initial import of stub code

---
+++ lib/MooseX/ClassAttribute.pm
@@ -0,0 +1,54 @@
+package MooseX::ClassAttribute;
+
+use warnings;
+use strict;
+
+our $VERSION = '0.01';
+
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+MooseX::ClassAttribute - The fantastic new MooseX::ClassAttribute!
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use MooseX::ClassAttribute;
+
+    my $foo = MooseX::ClassAttribute->new;
+
+    ...
+
+=head1 METHODS
+
+This class provides the following methods
+
+=head1 AUTHOR
+
+Dave Rolsky, C<< <autarch@urth.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-moosex-classattribute@rt.cpan.org>,
+or through the web interface at L<http://rt.cpan.org>.  I will be
+notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2007 Dave Rolsky, All Rights Reserved.
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
