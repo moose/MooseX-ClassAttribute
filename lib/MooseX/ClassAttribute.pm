fab23ffc5e4893382308bf2dbd84556e48df0b03
Dave Rolsky <autarch@urth.org>
2008-01-21 23:56:00 +0000

    Prep for 0.04 release

--- lib/MooseX/ClassAttribute.pm
+++ lib/MooseX/ClassAttribute.pm
@@ -3,7 +3,7 @@ package MooseX::ClassAttribute;
 use strict;
 use warnings;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 our $AUTHORITY = 'cpan:DROLSKY';
 
 our @EXPORT = 'class_has'; ## no critic ProhibitAutomaticExportation
